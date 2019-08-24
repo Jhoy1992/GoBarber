2416c5cb448cc451d3669b51e9f9b6f84ede8636
Jhoy1992 <jhonatanascari@gmail.com>
2019-08-24 16:37:44 -0300

    Configured nodemailer and send email when cancel an appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0ee03b9817ede7',
+    pass: 'b77f88e8fbb967',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
